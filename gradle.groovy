3ee2cc813862f6e60105f2330b8da8f7145cdb7d
Ach712 <vvce21cseaiml0024@vvce.ac.in>
2024-07-21 19:10:11 +0530

    Update gradle.groovy

--- gradle.groovy
+++ gradle.groovy
@@ -1,52 +1,50 @@
-import jenkins.model.*
-import hudson.model.*
-import hudson.tasks.*
-import hudson.plugins.git.*
-import hudson.plugins.gradle.*
-
-def jobName = "example"
-def repoUrl = "https://github.com/Ach712/gradle.git"
-def branch = "main"
-
-def job = Jenkins.instance.createProject(FreeStyleProject, jobName)
-
-// General configuration
-job.description = "Freestyle project for building a Gradle project"
-job.setDisplayName(jobName)
-
-// GitHub project URL without .git
-job.setProperty(new hudson.model.ParametersDefinitionProperty(new hudson.model.StringParameterDefinition('GITHUB_PROJECT_URL', repoUrl - '.git')))
-
-// Source Code Management
-def scm = new GitSCM(repoUrl)
-scm.branches = [new BranchSpec(branch)]
-job.scm = scm
-
-// Build Triggers
-def scmTrigger = new SCMTrigger('* * * * *')
-job.addTrigger(scmTrigger)
-
-// Build Steps - Invoke Gradle
-def gradle = new Gradle()
-gradle.tasks = 'clean build'
-gradle.useWrapper = true
-job.buildersList.add(gradle)
-
-// Build Steps - Execute shell (for Unix-like systems) or batch command (for Windows)
-if (System.getProperty('os.name').toLowerCase().contains('windows')) {
-    def batchCommand = new BatchFile('gradlew.bat run')
-    job.buildersList.add(batchCommand)
-} else {
-    def shellCommand = new Shell('chmod +x ./gradlew && ./gradlew run')
-    job.buildersList.add(shellCommand)
+job('example') {
+    // General configuration
+    properties {
+        githubProjectUrl('https://github.com/Ach712/gradle') // GitHub project URL without .git
+    }
+    
+    // Configure log rotation to keep the last 10 builds
+    logRotator {
+        numToKeep(10)
+    }
+    
+    // Use Java 17 JDK
+    jdk('Java 17')
+    
+    // Source Code Management
+    scm {
+        git {
+            remote {
+                url('git@github.com:Ach712/gradle.git')
+            }
+            branches('main')
+        }
+    }
+    
+    // Build Triggers
+    triggers {
+        scm('* * * * *')
+    }
+    
+    // Build Steps
+    steps {
+        gradle {
+            tasks('clean build') // Build the project using Gradle
+            useWrapper(true) // Use Gradle wrapper
+        }
+        
+        // Add a shell step to run the project
+        if (isUnix()) {
+            shell('chmod +x ./gradlew && ./gradlew run')
+        } else {
+            bat('gradlew.bat run')
+        }
+    }
+    
+    // Post-build Actions
+    publishers {
+        // Archive the built JAR file(s)
+        archiveArtifacts('build/libs/*.jar')
+    }
 }
-
-// Post-build Actions - Archive the artifacts
-def artifactArchiver = new ArtifactArchiver('build/libs/*.jar')
-job.publishersList.add(artifactArchiver)
-
-// Save the job configuration
-job.save()
-
-// Trigger a build
-job.scheduleBuild2(0)
